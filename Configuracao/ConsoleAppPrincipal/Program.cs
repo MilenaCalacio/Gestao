5c40be3fdc90c10523d7b3f42cbc0f1b4c770b9e
André Carlos Heringer <milenanascimento71140@gmail.com>
2023-02-15 21:56:04 -0300

    15-02 Atualizado
    Aula 15/02 Atualizado

--- Configuracao/ConsoleAppPrincipal/Program.cs
+++ Configuracao/ConsoleAppPrincipal/Program.cs
@@ -1,7 +1,57 @@
-﻿internal class Program
+﻿using BLL;
+using Models;
+
+
+internal class Program
 {
+
     private static void Main(string[] args)
     {
-        
+        int questao;
+
+        try
+        {
+
+            string opcao;
+            Usuario usuario = new Usuario();
+            UsuarioBLL usuarioBLL = new UsuarioBLL();
+
+
+            do
+            {
+
+                Console.WriteLine("informe o seu nome completo: ");
+                usuario.Nome = Console.ReadLine();
+
+
+                Console.WriteLine("informe o seu nome de usuario ");
+                usuario.NomeUsuario = Console.ReadLine();
+
+
+                Console.WriteLine("O usuário estará ativo (S) ou (N)");
+                usuario.Ativo = Console.ReadLine().ToUpper() == "S";
+
+                Console.WriteLine("informe o seu e-mail");
+                usuario.Email = Console.ReadLine();
+
+
+                Console.WriteLine("informe seu CPF");
+                usuario.CPF = Console.ReadLine();
+
+                Console.WriteLine("crie uma senha com no minimo sete digitos");
+                usuario.Senha = Console.ReadLine();
+
+                usuarioBLL.Inserir(usuario);
+
+                Console.WriteLine("Deseja fazer cadastro? [S] SIM [N] NAO");
+                questao = Convert.ToInt32(Console.ReadLine());
+            } while (questao == 1);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+
+        }
+
     }
 }
---
+++ Configuracao/DAL/PermissaoDAL.cs
@@ -0,0 +1,35 @@
+﻿
+using Models;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class PermissaoDAL
+    {
+        public void Inserir(Usuario usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+
+
+
+
+
+
+
+
+
+
+
+
+                public void Buscar();
+                public void Alterar();
+                public void Excluir();
+
+             }
+        }
