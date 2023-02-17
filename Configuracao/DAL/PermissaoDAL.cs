083082e7ed134f49f8219e2ee0faddb301fff581
André Carlos Heringer <milenanascimento71140@gmail.com>
2023-02-16 21:55:32 -0300

    16-02
    ATUALIZADO

--- Configuracao/DAL/PermissaoDAL.cs
+++ Configuracao/DAL/PermissaoDAL.cs
@@ -1,12 +1,11 @@
-﻿
-using Models;
+﻿using Models;
 using System.Data.SqlClient;
 
 namespace DAL
 {
     public class PermissaoDAL
     {
-        public void Inserir(Usuario usuario)
+        public void Inserir(Permissao permissao)
         {
             SqlConnection cn = new SqlConnection();
 
@@ -16,20 +15,40 @@ namespace DAL
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = cn;
 
+                cmd.CommandText = @"INSERT INTO Permissao (Desricao) 
+                                    VALUES(@Descricao)";
 
 
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Descricao",permissao.Descricao);
+                
 
+                cn.Open();
+                cmd.ExecuteScalar();
 
-
-
-
-
-
-
-
-                public void Buscar();
-                public void Alterar();
-                public void Excluir();
-
-             }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
+        public Usuario Buscar(string _nomeUsuario)
+        {
+            return new Usuario();
+        }
+        public void Alterar(Usuario _usuario)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+
+
+    }
+}
